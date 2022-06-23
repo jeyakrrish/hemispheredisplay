4359246b298cfd0b0ee33056b77e57bca8340deb
jeyakrrish <jeya.seelan1998@gmail.com>
2022-06-22 19:10:32 -0700

    state and SetState added

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    
+    constructor (props){
+        super(props);
+
+        this.state = {
+            latitude : 20
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+                this.setState({latitude : position.coords.latitude})
+            } ,
+            (error) => { 
+                console.log(error);
+                this.setState({latitude : error.message})
+            }
+        );
+
+    }
+   
     render() {
-// function App () {
-    window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error)
-        )   
         return (
-            <div>Laitude:</div>
+            <div>Laitude: {this.state.latitude} </div>
         )
     }
 }
