da07455d53bfdfed3a75716e1981f1f298b99e08
jeyakrrish <jeya.seelan1998@gmail.com>
2022-06-22 22:31:07 -0700

    some if else conditions added

--- src/index.js
+++ src/index.js
@@ -1,31 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import allow from './image/allow.png';
 
 class App extends React.Component {
     constructor (props){
         super(props);
 
         this.state = {
-            latitude : 20
+            latitude : null,
+            longitude : null,
+            errorMessage: '',
         };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 console.log(position);
-                this.setState({latitude : position.coords.latitude})
+                this.setState({latitude : position.coords.latitude + ' North'})
+                this.setState({longitude : position.coords.longitude + ' South'})
             } ,
             (error) => { 
                 console.log(error);
-                this.setState({latitude : error.message})
+                this.setState({errorMessage : error.message})
             }
         );
 
     }
    
     render() {
-        return (
-            <div>Laitude: {this.state.latitude} </div>
-        )
+        if (this.state.latitude && !this.state.errorMessage){
+            return (
+                <div style= {{margin:'100px auto', width:'50%', textAlign:'center', lineHeight:1.25}}>
+                    <div style={{borderRadius:'4px' ,border:'1px solid gray', padding:'10px'}}>
+                        <h1>Know your device location</h1>
+                        <p> Latitude: <q>{this.state.latitude}</q> <br/> Longitude: <q>{this.state.longitude}</q> </p>
+                    </div>
+                </div>
+            )
+        }
+        if (!this.state.latitude && this.state.errorMessage) {
+            return (
+                <div style= {{margin:'100px auto', width:'50%', textAlign:'center', lineHeight:1.25}}>
+                    <div style={{borderRadius:'4px' ,border:'1px solid gray', padding:'10px'}}>
+                        <h1>Know your device location</h1>
+                        <p>{this.state.errorMessage}</p>
+                        <p><i>Allow location permission to know your location</i></p>
+                        <img width='240px' src={allow} alt='permission' />
+                    </div>
+                </div> 
+            )
+        } else {
+            return (
+                <div style= {{margin:'100px auto', width:'50%', textAlign:'center', lineHeight:1.25}}>
+                    <div style={{borderRadius:'4px' ,border:'1px solid gray', padding:'10px'}}>
+                        <h1>Know your device location</h1>
+                        <p> Latitude: Loading... <br/> Longitude: Loading... </p>
+                    </div>
+                </div> 
+            )
+        }
     }
 }
 
